bfd7c980786d12f88c3cea6d93ae96de8b08d04f
Vigilancia Cochera Paraguay <vcocheraParaguay@bcr.com.ar>
2022-06-01 07:36:19 -0300

    copia terminada

---
+++ index.js
@@ -0,0 +1,10 @@
+$("h1").click(function() {
+  $("h1").css("color", "red");
+});
+
+$(".btn1").click(function() {
+  $(".series").slideToggle();
+});
+$(".DBZ").click(function() {
+  $(".sagas").slideToggle();
+});
